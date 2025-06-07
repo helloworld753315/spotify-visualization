b99bdd9325ce99c6cabd9dac9a4e483a9abcba55
helloworld753315 <helloworld.cpp.h@gmail.com>
2025-06-07 10:19:32 +0900

    add ボタンコンポーネントにクリックイベントを追加し、Spotifyログインボタンを新規作成

--- app/src/app/_components/Button.tsx
+++ app/src/app/_components/Button.tsx
@@ -3,13 +3,20 @@ import styles from '@styles/button.module.scss'
 type Props = {
   title?: string
   children: string
-  color?:string
-};
+  color?: string
+  onClick?: () => void // クリックイベントを受け取るプロパティを追加
+}
 
 export default function Button(props: Props) {
   return (
     <div>
-      <button className={styles.button}>{props.children}</button>
+      <button
+        className={styles.button}
+        style={{ backgroundColor: props.color }}
+        onClick={props.onClick} // クリックイベントを設定
+      >
+        {props.children}
+      </button>
     </div>
-  );
+  )
 }
